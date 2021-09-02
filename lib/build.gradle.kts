66a1b6a145588816932097a74be3a4dbb319a696
Marquis Kurt <software@marquiskurt.net>
2021-09-02 09:45:32 -0400

    :bookmark: Add project version strings for 1.0-SNAPSHOT
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,6 +6,8 @@
  * User Manual available at https://docs.gradle.org/7.2/userguide/building_java_projects.html
  */
 
+project.version = "1.0-SNAPSHOT"
+
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.4.32"
